6f7ea2b5d787ed75dc31c69dec8463a37f347556
ISAQUE25-INFORMATICA <161869808+ISAQUE25-INFORMATICA@users.noreply.github.com>
2024-03-08 15:07:24 -0300

    Create bob.cpp

---
+++ bob.cpp
@@ -0,0 +1,45 @@
+#include
+using namespace std;
+
+int main(){
+
+cout << "\n\t (¨¨)¨¨)¨) VAMOS COZINHAR BEEEEEEEEEEE";
+cout << "\n\t |(|(|/";
+cout << "\n\t (_ /";
+cout << "\n\t | |";
+cout << "\n\t | | |¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨|";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t | | | |";
+cout << "\n\t (¨¨¨)| |";
+cout << "\n\t | || |";
+cout << "\n\t (/| |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t | |";
+cout << "\n\t ||";
+cout << "\n\t | \ / \/ \ / |";
+cout << "\n\t | \ / / \ \ / |";
+cout << "\n\t | V / \ V |";
+cout << "\n\t | \ / |";
+cout << "\n\t |\ /|";
+cout << "\n\t | \ / |";
+cout << "\n\t | \/ |";
+cout << "\n\t | ||||||||| ||||||||| |";
+cout << "\n\t |_____________________________________________________|";
+cout << "\n\t / \ ";
+
+}
